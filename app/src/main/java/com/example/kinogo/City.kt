44152057d89edac6880c5e02b6cad2fd0c0c5bba
vakur1 <temavakurov2@icloud.com>
2022-12-06 21:33:39 +0300

    первое дз

---
+++ app/src/main/java/com/example/kinogo/City.kt
@@ -0,0 +1,3 @@
+package com.example.kinogo
+
+data class City(val town : String, var temperature : Int )
--- app/src/main/java/com/example/kinogo/MainActivity.kt
+++ app/src/main/java/com/example/kinogo/MainActivity.kt
@@ -3,13 +3,32 @@ package com.example.kinogo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<Button>(R.id.button)
+        val textview = findViewById<TextView>(R.id.textview)
+        val textviewcopy = findViewById<TextView>(R.id.textviewcopy)
+        val buttoncopy : Button = findViewById(R.id.buttoncopy)
 
+        val Moscow = City("Moscow", 0)
+        Moscow.temperature = 0
+        textview.text = "Saint Petersburg (-5)"
+
+        button.setOnClickListener{
+            textview.text = Moscow.town + " (" + Moscow.temperature + ")"
+        }
+
+        buttoncopy.setOnClickListener{
+            val objectView = object {
+                val MSKcopy = Moscow.copy()
+            }
+            textviewcopy.text = objectView.MSKcopy.town + " (" + Moscow.temperature + ")"
+        }
 
     }
 }
