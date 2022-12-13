8e8788ad7afbfbd6db064ede50e2bcfd34670c83
vakur1 <temavakurov2@icloud.com>
2022-12-13 19:08:02 +0300

    добавил viewmodel

--- app/src/main/java/com/example/kinogo/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/kinogo/viewModel/MainViewModel.kt
@@ -2,9 +2,30 @@ package com.example.kinogo.viewModel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kinogo.data.Repository
+import com.example.kinogo.data.RepositoryImpl
 import com.example.kinogo.repository.AppState
 
-class MainViewModel (private val liveDataToObserve : MutableLiveData<AppState> = MutableLiveData())
-    : ViewModel() {
+class MainViewModel(
+    private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(),
+    private val repositoryImpl: Repository = RepositoryImpl()
+) : ViewModel() {
 
+
+    fun getLiveData() = liveDataToObserve;
+    fun getMoviesFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            Thread.sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getMoviesFromLocalStorage()))
+        }.start()
+    }
+
+    fun getMoviesFromRemoteSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            Thread.sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getMoviesFromServer()))
+        }.start()
+    }
 }
