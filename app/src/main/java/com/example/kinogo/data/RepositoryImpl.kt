b2e592635678d9ca4a9cd43081fa77f8d070f324
vakur1 <temavakurov2@icloud.com>
2022-12-13 18:53:44 +0300

    Добавил вьюмодель

--- app/src/main/java/com/example/kinogo/data/RepositoryImpl.kt
+++ app/src/main/java/com/example/kinogo/data/RepositoryImpl.kt
@@ -2,24 +2,20 @@ package com.example.kinogo.data
 
 class RepositoryImpl : Repository {
 
-    private lateinit var categories: List<Category>
-    private lateinit var movies: List<Movie>
+    private var movies: List<Movie> = listOf(
+        Movie("Интерстеллар", 0, 86, "08 июль 2014"),//фантастика
+        Movie("1+1", 1, 88, "07 июл 2011"),//драма
+        Movie("Начало", 2, 78, "30 июн 2010"),//фантакстика
+        Movie("Джентельмены", 3, 79, "01 июл 2019"),//криминал
+        Movie("Зверополис", 4, 82, "30 июн 2016")
+    )
 
-    init {
-        movies = listOf(
-            Movie("Интерстеллар", 0, 86, "08 июль 2014"),//фантастика
-            Movie("1+1", 1, 88, "07 июл 2011"),//драма
-            Movie("Начало", 2, 78, "30 июн 2010"),//фантакстика
-            Movie("Джентельмены", 3, 79, "01 июл 2019"),//криминал
-            Movie("Зверополис", 4, 82, "30 июн 2016")
-        )
-        categories = listOf(
-            Category("Фантастика", movies),
-            Category("Драма", movies),
-            Category("Криминал", movies),
-            Category("Мультфильм", movies),
-        )
-    }
+    private var categories: List<Category> = listOf(
+        Category("Фантастика", movies),
+        Category("Драма", movies),
+        Category("Криминал", movies),
+        Category("Мультфильм", movies),
+    )
 
     override fun getMoviesFromServer(): List<Category> = categories
     override fun getMoviesFromLocalStorage(): List<Category> = categories
--- app/src/main/java/com/example/kinogo/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/kinogo/viewModel/MainViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.kinogo.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.kinogo.repository.AppState
 
-class MainViewModel : ViewModel() {
+class MainViewModel (private val liveDataToObserve : MutableLiveData<AppState> = MutableLiveData())
+    : ViewModel() {
+
 }
