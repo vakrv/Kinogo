9db4060f7096ece13ec13cb26ccc4d9bd2c289f7
Artem Vakurov <temavakurov2@icloud.com>
2022-12-17 14:17:18 +0300

    откатил всё

---
+++ app/src/main/java/com/example/kinogo/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kinogo.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kinogo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(com.google.android.material.R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kinogo/view/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.example.kinogo.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.kinogo.R
+import com.example.kinogo.viewModel.MainViewModel
+
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel : MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        // TODO: Use the ViewModel
+    }
+}
